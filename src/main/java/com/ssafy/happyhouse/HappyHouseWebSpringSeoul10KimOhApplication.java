081ec811923efc413a57a4ad602170b0b47e390e
USER <USER@192.168.25.53>
2020-06-01 15:36:19 +0900

    happyhouse

---
+++ src/main/java/com/ssafy/happyhouse/HappyHouseWebSpringSeoul10KimOhApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.happyhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyHouseWebSpringSeoul10KimOhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyHouseWebSpringSeoul10KimOhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/happyhouse/HappyHouseWebSpringSeoul10KimOhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.happyhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyHouseWebSpringSeoul10KimOhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
